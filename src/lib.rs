591f2bf0bcb505b568fd3122820fbf41d68346c9
Pete LeVasseur <plevasseur@gmail.com>
2025-12-27 02:57:53 +0900

    feat: add trait and show method-style

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,27 @@
 #![no_std]
-use itertools::zip_eq;
+use itertools::Itertools;
+use itertools::zip_eq; // Need the trait for method-style .zip_eq()
+
+pub struct Container<const N: usize> {
+    array: [i32; N],
+}
+
+impl<const N: usize> Container<N> {
+    pub const fn new() -> Self {
+        Self { array: [0; N] }
+    }
+
+    pub fn from_iter(&mut self, iter: impl IntoIterator<Item = i32>) {
+        for (val, dest) in iter.into_iter().zip_eq(self.array.iter_mut()) {
+            *dest = val;
+        }
+    }
+}
+
+pub fn test_container() {
+    let mut container = Container::<5>::new();
+    container.from_iter([1, 2, 3, 4, 5]);
+}
 
 pub fn test() {
     let data = [1, 2, 3, 4, 5];
