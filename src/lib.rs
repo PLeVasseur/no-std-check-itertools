a87a4d56ae62415bb95c3e34bb0af190e75c57c0
Pete LeVasseur <plevasseur@gmail.com>
2025-12-27 02:44:39 +0900

    feat: show we can use zip_eq from itertools in a no_std context

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
 #![no_std]
+use itertools::zip_eq;
 
-pub fn test() {}
+pub fn test() {
+    let data = [1, 2, 3, 4, 5];
+    for (a, b) in zip_eq(&data[..data.len() - 1], &data[1..]) {
+        let _c = a + b;
+    }
+}
